6a1540a73e5038457d843d9dec94d5235b8477ca
vagrant <vagrant@localhost.localdomain>
2018-01-09 14:26:54 +0000

    Init commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+ action:install
+end
+
+package 'ntp' 
+
+package 'git' do
+ action:install
+end
+
+file '/etc/motd' do 
+ content 'This server is the property of Tech'
+ action:create
+ owner 'root'
+ group 'root'
+end
+
+
